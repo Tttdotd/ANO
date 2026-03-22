a4bbe119a122e44954f12ba54a48923a3c9df366
Kangkang <rundong.zhang350@gmail.com>
2026-03-22 20:48:48 +0800

    构建项目框架, 创建rules, 导入api文档及数据库构建脚本.

---
+++ src/main/java/com/tdotd/ano/AnoApplication.java
@@ -0,0 +1,13 @@
+package com.tdotd.ano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdotd/ano/AnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdotd.ano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
